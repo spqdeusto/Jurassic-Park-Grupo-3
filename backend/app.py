c46a41c82e8b07ab750364078904504e98980b76
Paul <paul.hernandez@opendeusto.es>
2022-11-14 18:47:21 +0100

    vue initialization

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from routes.dinosaur import dinosaur
 from config.openapi import tags_metadata
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(
     title="Dinosaurs API",
@@ -8,5 +9,18 @@ app = FastAPI(
     version="0.0.1",
     openapi_tags=tags_metadata,
 )
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:8080"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.get("/")
+def home():
+    return "Hello, World!"
 
 app.include_router(dinosaur)
