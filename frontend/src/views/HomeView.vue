3b4500612a75e3eec6f3931f7d791d08846109b4
anebollo <anebollo@gmail.com>
2022-12-13 19:16:19 +0100

    homeview

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -3,22 +3,10 @@
     <img alt="Vue logo" src="../assets/jurassicparklogo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
-      <nav>
-    <router-link to="/dinomenu">DINO</router-link> |
-    <router-link to="/jeepmenu">JEEP</router-link> |
-    <router-link to="/recintomenu">RECINTO</router-link> |
-    </nav>
-
+  <button onclick="window.location.href='/dinomenu'" style="float: center;width: 100px; height: 50px;" >Menu Dino</button>
+    <button onclick="window.location.href='/jeepmenu'" style="float: center;width: 100px; height: 50px;" >Menu Jeep</button>
+    <button onclick="window.location.href='/recintomenu'" style="float: center;width: 100px; height: 50px;" >Menu Recinto</button>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
-export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
-}
-</script>
+
