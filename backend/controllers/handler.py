461223ac3cb18f86f0c32d357eaced7d209537fd
anebollo <anebollo@gmail.com>
2022-12-18 15:26:52 +0100

    documentacion

--- backend/controllers/handler.py
+++ backend/controllers/handler.py
@@ -6,10 +6,25 @@ import backend.utils.vars as gb
 from sqlalchemy.orm import Session
 
 class Controllers:
+  """
+    Esta clase proporciona métodos para interactuar con la base de datos y realizar
+  diversas operaciones relacionadas con dinosaurios y recintos.
+  """
   def __init__(self) -> None:
+    """
+      Constructor de la clase.
+
+      @return: None
+    """
     pass
     
   def get_dinos(self):
+
+    """
+      Recupera todos los dinosaurios de la base de datos.
+
+      @return: Una lista de objetos Dinosaur.
+    """
 
     db = DatabaseClient(gb.MYSQL_URL)
     with Session(db.engine) as session:
@@ -18,6 +33,15 @@ class Controllers:
     return dinos
 
   def startRuta(self, body: models.GetRequest):
+
+    """
+      Cambia el valor del campo "ruta" a 1 para el TodoTerreno especificado
+      en el cuerpo de la solicitud. Luego, llama al método check_status y
+      devuelve el resultado.
+
+      @param body: Un objeto GetRequest que contiene el ID del TodoTerreno.
+      @return: El resultado del método check_status.
+    """
     
     db = DatabaseClient(gb.MYSQL_URL)
     with Session(db.engine) as session:
@@ -28,6 +52,16 @@ class Controllers:
     return {"status": "ok"}
   
   def quitRuta(self, body: models.GetRequest):
+
+    """
+      Cambia el valor del campo "ruta" a 0 para el TodoTerreno especificado
+      en el cuerpo de la solicitud. Luego, llama al método jeepStateDown con
+      el ID del TodoTerreno especificado y finalmente devuelve el resultado
+      del método check_status.
+
+      @param body: Un objeto GetRequest que contiene el ID del TodoTerreno.
+      @return: El resultado del método check_status.
+    """
     
     db = DatabaseClient(gb.MYSQL_URL)
     with Session(db.engine) as session:
@@ -39,6 +73,13 @@ class Controllers:
     return {"status": "ok"}
   
   def get_recintos(self):
+    """
+      Recupera todos los recintos de la base de datos y para cada recinto,
+      recupera la lista de dinosaurios que están asignados a ese recinto.
+
+      @return: Una lista de objetos Recinto con una propiedad adicional "dinosaurs"
+                que contiene la lista de dinosaurios asociados al recinto.
+    """
 
     db = DatabaseClient(gb.MYSQL_URL)
     with Session(db.engine) as session:
@@ -50,6 +91,11 @@ class Controllers:
     return recintos
   
   def dangerousness(self):
+    """
+    Comprueba si hay algún dinosaurio peligroso en algún recinto apagado.
+
+    @return: True si hay algún dinosaurio peligroso en algún recinto apagado, False en caso contrario.
+    """
 
     recintos = self.get_recintos()
 
@@ -62,6 +108,11 @@ class Controllers:
     return False
   
   def get_jeeps(self):
+    """
+    Recupera todos los TodoTerreno de la base de datos.
+
+    @return: Una lista de objetos TodoTerreno.
+    """
 
     db = DatabaseClient(gb.MYSQL_URL)
     with Session(db.engine) as session:
@@ -71,6 +122,11 @@ class Controllers:
     return jeeps
 
   def jeepsEnRuta(self):
+    """
+    Recupera todos los TodoTerreno que están en ruta.
+
+    @return: Una lista de objetos TodoTerreno que están en ruta.
+    """
     
     enRuta = []
     jeeps = self.get_jeeps()
@@ -82,6 +138,11 @@ class Controllers:
     return enRuta
 
   def jeepsNoEnRuta(self):
+    """
+    Recupera todos los TodoTerreno que no están en ruta.
+
+    @return: Una lista de objetos TodoTerreno que no están en ruta.
+    """
     
     NoenRuta = []
     jeeps = self.get_jeeps()
@@ -93,6 +154,11 @@ class Controllers:
     return NoenRuta
 
   def recintosApagados(self):
+    """
+    Recupera todos los recintos apagados.
+
+    @return: Una lista de objetos Recinto que están apagados.
+    """
     
     apagados = []
     recintos = self.get_recintos()
@@ -104,6 +170,11 @@ class Controllers:
     return apagados
 
   def recintosEncendidos(self):
+    """
+    Recupera todos los recintos encendidos.
+
+    @return: Una lista de objetos Recinto que están encendidos.
+    """
     
     encendidos = []
     recintos = self.get_recintos()
@@ -115,6 +186,11 @@ class Controllers:
     return encendidos
     
   def jeepStateUp(self):
+    """
+    Cambia el valor del campo "sistemaseguridad" a 1 para todos los TodoTerreno que estén en ruta.
+
+    @return: None
+    """
 
     enRuta = self.jeepsEnRuta()
     db = DatabaseClient(gb.MYSQL_URL)
@@ -126,6 +202,12 @@ class Controllers:
     return {"status": "ok"}
 
   def jeepStateDownAll(self):
+    """
+    Apaga el sistema de seguridad de todos los Jeep que se encuentran en ruta.
+    
+    @param self: objeto de la clase actual
+    @return: un diccionario con el estado de la operación
+    """
 
     enRuta = self.jeepsEnRuta()
     db = DatabaseClient(gb.MYSQL_URL)
@@ -137,6 +219,12 @@ class Controllers:
     return {"status": "ok"}
   
   def jeepStateDown(self, id: int):
+    """
+    Cambia el valor del campo "sistemaseguridad" a 0 para el TodoTerreno con el ID especificado.
+
+    @param id: El ID del TodoTerreno a actualizar.
+    @return: None
+    """
 
     db = DatabaseClient(gb.MYSQL_URL)
     with Session(db.engine) as session:
@@ -146,6 +234,11 @@ class Controllers:
     return {"status": "ok"}
   
   def check_electricity(self):
+    """
+    Verifica si hay algún recinto apagado.
+
+    @return: True si hay al menos un recinto apagado, False en caso contrario.
+    """
     
     for recinto in self.get_recintos():
       if(recinto.state==False):
@@ -153,6 +246,14 @@ class Controllers:
     return True
   
   def check_status(self):
+    """
+    Verifica el estado del parque y devuelve información sobre él.
+
+    @return: Un diccionario con la información del estado del parque. La clave "alerta"
+             indica el nivel de alerta actual (puede ser "normal", "baja", "media" o "maxima"),
+             las claves "recintos" y "jeeps" contienen listas de objetos Recinto y TodoTerreno,
+             respectivamente.
+    """
     
     if(self.dangerousness() and self.jeepsEnRuta()):
         self.jeepStateUp()
@@ -173,7 +274,13 @@ class Controllers:
   
   def quit_electricity(self, body: models.GetRequest):
     """
-    Remove electricity from the wall of a given enclosure
+    Quita la electricidad de la pared de un recinto específico.
+
+    @param body: El objeto GetRequest que contiene el ID del recinto.
+    @return: Un diccionario con la información del estado del parque después de quitar la electricidad.
+             La clave "alerta" indica el nivel de alerta actual (puede ser "normal", "baja", "media" o "maxima"),
+             las claves "recintos" y "jeeps" contienen listas de objetos Recinto y TodoTerreno,
+             respectivamente.
     """
     db = DatabaseClient(gb.MYSQL_URL)
     with Session(db.engine) as session:
@@ -185,7 +292,13 @@ class Controllers:
   
   def put_electricity(self, body: models.GetRequest):
     """
-    Remove electricity from the wall of a given enclosure
+    Pone la electricidad de la pared de un recinto específico.
+
+    @param body: El objeto GetRequest que contiene el ID del recinto.
+    @return: Un diccionario con la información del estado del parque después de poner la electricidad.
+             La clave "alerta" indica el nivel de alerta actual (puede ser "normal", "baja", "media" o "maxima"),
+             las claves "recintos" y "jeeps" contienen listas de objetos Recinto y TodoTerreno,
+             respectivamente.
     """
     db = DatabaseClient(gb.MYSQL_URL)
     with Session(db.engine) as session:
@@ -197,7 +310,10 @@ class Controllers:
   
   def create_recinto(self, body: models.Recinto):
     """
-    Creates new recinto in the database
+    Crea un nuevo recinto en la base de datos.
+
+    @param body: El objeto Recinto que contiene la información del recinto a crear.
+    @return: None
     """
     body_row = mysql_models.Recinto(name=body.name, state=body.state)
 
@@ -211,7 +327,10 @@ class Controllers:
   
   def create_dinosaur(self, body: models.Dinosaur):
     """
-    Creates new dinosaur in the database
+    Crea un nuevo dinosaurio en la base de datos.
+
+    @param body: El objeto Dinosaur que contiene la información del dinosaurio a crear.
+    @return: Un diccionario con una clave "status" y un valor "ok" en caso de éxito.
     """
     print(body)
     body_row = mysql_models.Dinosaur(name=body.name, species=body.species, age=body.age, weigh=body.weigh, gender=body.gender, dangerousness=body.dangerousness, recinto=body.recinto)
@@ -226,7 +345,10 @@ class Controllers:
   
   def create_todoterreno(self, body: models.TodoTerreno):
     """
-    Creates new todoterreno in the database
+    Crea un nuevo todoterreno en la base de datos.
+
+    @param body: El objeto TodoTerreno que contiene la información del todoterreno a crear.
+    @return: Un diccionario con una clave "status" y un valor "ok" en caso de éxito.
     """
     body_row = mysql_models.TodoTerreno(ruta=body.ruta, numvisitantes=body.numvisitantes, sistemaseguridad=body.sistemaseguridad)
 
@@ -240,7 +362,12 @@ class Controllers:
   
   def delete_recinto(self, body: models.DeleteRequest):
     """
-    Deletes recinto by its UID
+    Elimina un recinto por su UID.
+    
+    @param body: Un objeto DeleteRequest que contiene el UID del recinto a eliminar.
+    @type body: models.DeleteRequest
+    @return: Un diccionario que indica el estado de la operación.
+    @rtype: dict
     """
     db = DatabaseClient(gb.MYSQL_URL)
     with Session(db.engine) as session:
@@ -253,7 +380,12 @@ class Controllers:
 
   def delete_dinosaur(self, body: models.DeleteRequest):
     """
-    Deletes dinosaur by its UID
+    Elimina un dinosaurio por su UID.
+    
+    @param body: Un objeto DeleteRequest que contiene el UID del dinosaurio a eliminar.
+    @type body: models.DeleteRequest
+    @return: Un diccionario que indica el estado de la operación.
+    @rtype: dict
     """
     db = DatabaseClient(gb.MYSQL_URL)
     with Session(db.engine) as session:
@@ -266,7 +398,12 @@ class Controllers:
 
   def delete_todoterreno(self, body: models.DeleteRequest):
     """
-    Deletes todoterreno by its UID
+    Elimina un todoterreno por su UID.
+    
+    @param body: Un objeto DeleteRequest que contiene el UID del todoterreno a eliminar.
+    @type body: models.DeleteRequest
+    @return: Un diccionario que indica el estado de la operación.
+    @rtype: dict
     """
     db = DatabaseClient(gb.MYSQL_URL)
     with Session(db.engine) as session:
@@ -279,7 +416,12 @@ class Controllers:
   
   def update_recinto(self, body: models.UpdateRecinto):
     """
-    Updates recinto by its ID
+    Actualiza un recinto por su ID.
+    
+    @param body: Un objeto UpdateRecinto que contiene el ID y la información actualizada del recinto.
+    @type body: models.UpdateRecinto
+    @return: Un diccionario que indica el estado de la operación.
+    @rtype: dict
     """
     db = DatabaseClient(gb.MYSQL_URL)
     with Session(db.engine) as session:
@@ -294,7 +436,12 @@ class Controllers:
 
   def update_dinosaur(self, body: models.UpdateDinosaur):
     """
-    Updates dinosaur by its ID
+    Actualiza un dinosaurio por su ID.
+    
+    @param body: Un objeto UpdateDinosaur que contiene el ID y la información actualizada del dinosaurio.
+    @type body: models.UpdateDinosaur
+    @return: Un diccionario que indica el estado de la operación.
+    @rtype: dict
     """
     db = DatabaseClient(gb.MYSQL_URL)
     with Session(db.engine) as session:
@@ -313,7 +460,12 @@ class Controllers:
   
   def update_todoterreno(self, body: models.UpdateTodoTerreno):
     """
-    Updates todoterreno by its ID
+    Actualiza un todoterreno por su ID.
+    
+    @param body: Un objeto UpdateTodoTerreno que contiene el ID y la información actualizada del todoterreno.
+    @type body: models.UpdateTodoTerreno
+    @return: Un diccionario que indica el estado de la operación.
+    @rtype: dict
     """
     db = DatabaseClient(gb.MYSQL_URL)
     with Session(db.engine) as session:
