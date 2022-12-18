77be6b4c0e54a5beb62c03a90439b4b09524791a
anebollo <anebollo@gmail.com>
2022-12-18 15:54:23 +0100

    docuemtnacion

--- backend/mysql/models.py
+++ backend/mysql/models.py
@@ -15,6 +15,20 @@ class Especie(Base):
     )
 """
 class Dinosaur(Base):
+  """
+    Esta clase representa a un dinosaurio en la base de datos.
+    
+    @param id (int): el ID único del dinosaurio en la base de datos.
+    @param name (str): el nombre del dinosaurio.
+    @param species (str): la especie a la que pertenece el dinosaurio.
+    @param age (int): la edad del dinosaurio.
+    @param weigh (int): el peso del dinosaurio.
+    @param gender (bool): un valor booleano que indica si el dinosaurio es macho (True) o hembra (False).
+    @param dangerousness (bool): un valor booleano que indica si el dinosaurio es agresivo (True) o pacífico (False).
+    @param recinto (int): el ID del recinto al que pertenece el dinosaurio.
+    
+    @return: un objeto que representa al dinosaurio.
+    """
   __tablename__ = "dinosaurs"
   id = Column(Integer, Sequence("user_id_seq"), primary_key=True)
   name = Column(String(100))
@@ -28,6 +42,11 @@ class Dinosaur(Base):
   recinto = Column(Integer, ForeignKey("recinto.id"), nullable=False)
 
   def __repr__(self) -> str:
+    """
+        Devuelve una representación en cadena del dinosaurio.
+        
+        @return: una cadena que representa al dinosaurio.
+        """
     return "<Dinosaur(id= '%d', name='%s', species='%s', age='%d', weigh='%d', gender='%b', dangerousness='%b',recinto='%d')>" % (
       self.id,
       self.name,
@@ -40,6 +59,16 @@ class Dinosaur(Base):
     )
 
 class Recinto(Base):
+  """
+    Esta clase representa un recinto en la base de datos.
+    
+    @param id (int): el ID único del recinto en la base de datos.
+    @param name (str): el nombre del recinto.
+    @param state (bool): un valor booleano que indica si el recinto está abierto (True) o cerrado (False).
+    @param dinosaurs (list): una lista de objetos Dinosaur que pertenecen al recinto.
+    
+    @return: un objeto que representa al recinto.
+    """
   __tablename__ = "recinto"
   id = Column(Integer, Sequence("user_id_seq"), primary_key=True)
   name = Column(String(100))
@@ -47,6 +76,11 @@ class Recinto(Base):
   dinosaurs = relationship("Dinosaur", cascade="all, delete")
   
   def __repr__(self) -> str:
+    """
+        Devuelve una representación en cadena del recinto.
+        
+        @return: una cadena que representa al recinto.
+        """
     return "<Recinto(id= '%d', name='%s', state='%b')>" % (
       self.id,
       self.name,
@@ -54,6 +88,14 @@ class Recinto(Base):
     )
 
 class TodoTerreno(Base):
+  """
+  Clase que representa un todo terreno.
+  
+  @param id (int): Identificador único del todo terreno.
+  @param ruta (bool): Indica si el todo terreno es apto para recorrer rutas.
+  @param  numvisitantes (int): Número de visitantes que puede llevar el todo terreno.
+  @param  sistemaseguridad (bool): Indica si el todo terreno cuenta con sistema de seguridad.
+  """
   __tablename__ = "todoterreno"
   id = Column(Integer, Sequence("user_id_seq"), primary_key=True)
   ruta=Column(Boolean(create_constraint=True), nullable=False, default=False)
@@ -61,6 +103,11 @@ class TodoTerreno(Base):
   sistemaseguridad=Column(Boolean(create_constraint=True), nullable=False, default=False)
 
   def __repr__(self) -> str:
+    """
+    Devuelve una representación en forma de cadena del todo terreno.
+    
+    @return Representación en forma de cadena del todoterreno.
+    """
     return "<TodoTerreno(id= '%d', ruta='%b', numvisitantes='%d', sistemaseguridad='%b')>" % (
       self.id,
       self.ruta,
