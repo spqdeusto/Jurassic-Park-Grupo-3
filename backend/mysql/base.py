a5a76d447a054f2a9d40cb23119f1b2e9aeca382
anebollo <anebollo@gmail.com>
2022-12-18 16:06:16 +0100

    documentacion backend

--- backend/mysql/base.py
+++ backend/mysql/base.py
@@ -1,3 +1,9 @@
 from sqlalchemy.orm import declarative_base
 
+"""
+    La clase Base creada a partir de declarative_base se utilizará para definir las clases 
+    que representan las tablas de la base de datos y para realizar operaciones de mapeo 
+    objeto-relacional (ORM) en la base de datos.
+"""
+
 Base = declarative_base()
--- backend/utils/vars.py
+++ backend/utils/vars.py
@@ -1,3 +1,11 @@
+"""
+    Se está importando la biblioteca os y se está definiendo una constante MYSQL_URL que 
+    se inicializa con una cadena de texto que representa la URL de una base de datos MySQL.
+
+    La biblioteca os se utiliza para acceder a las variables de entorno del sistema operativo 
+    en el que se está ejecutando el código. El método os.getenv() se utiliza para obtener el valor 
+    de una variable de entorno específica. Si la variable de entorno no existe, os.getenv() devuelve None.
+"""
 import os
 
 MYSQL_URL: str = os.getenv("MYSQL_URL") if os.getenv("MYSQL_URL") != None  else "mysql://admin_db:admin_db@0.0.0.0:3306/fastapi"
